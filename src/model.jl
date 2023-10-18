22922e3cd8e1f7a90c9323b4064b9a07242d7488
Ole <ol02@gmx.net>
2023-10-18 16:33:21 +0200

    added CRF as an input

--- src/model.jl
+++ src/model.jl
@@ -1,6 +1,10 @@
 function solve_model(Optimizer)
     model = Model(Optimizer);
 
+    # variables
+
+    CRF = 1;  # capital recovery factor
+
     # variables of the model
     
     @variable(model, EAC);  # total annualised cost of the energy systems
@@ -8,13 +12,12 @@ function solve_model(Optimizer)
     @variable(model, NPV_CF >= 0);  # net present value of the yearly cash flows
     @variable(model, NPV_ReInv >= 0);  # net present value of the reinvestments
     @variable(model, NPV_ResV >= 0);  # net present value of the residual values
-    @variable(model, CRF >= 0);  # capital recovery factor
 
     # objective of the model
     @objective(model, Min, EAC);
     
     # constraints of the model
-   # @constraint(model, EAC == (CAPEX - NPV_CF + NPV_ReInv - NPV_ResV) * CRF);  # calculating EAC
+    @constraint(model, EAC >= (CAPEX - NPV_CF + NPV_ReInv - NPV_ResV) * CRF);  # calculating EAC
     print("in here ");
     return 1;
 end
