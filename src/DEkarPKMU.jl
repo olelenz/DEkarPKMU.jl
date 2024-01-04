b890cf0806b786b918209e9159c340b6be9d3008
Ole <ol02@gmx.net>
2024-01-04 18:05:04 +0100

    ready to merge into main

--- src/DEkarPKMU.jl
+++ src/DEkarPKMU.jl
@@ -11,17 +11,8 @@ include(joinpath(@__DIR__, "input.jl"));
 include(joinpath(@__DIR__, "model.jl"));
 include(joinpath(@__DIR__, "pdfgen.jl"));
 
-
-function test()
-    #data::Data = initData(initSampleJSON());
-    data::Data = initDataXLSX("/Users/ole/Documents/Uni/WS2324/POM/master_thesis/230717_Energiesystemmodellierung_Input_Output.xlsx");
-    model = solve_model_fast(HiGHS.Optimizer, data);
-    #print(model);  # overview of the solved model
-    
-    # TODO: create output visualisations
-    # TODO: return structured result
-    return model;
-end 
-
+function modelTest()
+    generatePdfTest();
+end
 
 end # module DEkarPKMU
